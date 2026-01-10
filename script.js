eae10b6d9a540f9ec4d7bdfe28e10593703e62e7
Jallen Hunnid <aubinderulo@gmail.com>
2026-01-10 13:06:20 +0200

    Final Project

--- script.js
+++ script.js
@@ -3,8 +3,7 @@
 const containerDiv = document.querySelector('.container')
 const headerTitle = document.createElement('h1');
 const gameIcon = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><title>nintendo-game-boy</title><path d="M7 1C5.9 1 5 1.9 5 3V21C5 22.11 5.9 23 7 23H14C16.76 23 19 20.76 19 18V3C19 1.9 18.11 1 17 1H7M8 4H16V11H8V4M9 14H10V16H12V17H10V19H9V17H7V16H9V14M16 15C16.55 15 17 15.45 17 16C17 16.55 16.55 17 16 17C15.45 17 15 16.55 15 16C15 15.45 15.45 15 16 15M14 17C14.55 17 15 17.45 15 18C15 18.55 14.55 19 14 19C13.45 19 13 18.55 13 18C13 17.45 13.45 17 14 17Z" /></svg>`
-headerTitle.innerHTML = 'Tic Tac Toe Game ' + gameIcon 
-
+headerTitle.innerHTML = 'Tic-Tac-Toe Game ' + gameIcon + `<br><i>(Jallen's Touch!)</i>`; 
 const sideBarDiv = document.createElement('div');
 
 sideBarDiv.classList.add ('sideBar');
@@ -17,6 +16,12 @@ const startButton = document.createElement('button')
 const startIcon = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><title>power-standby</title><path d="M13,3H11V13H13V3M17.83,5.17L16.41,6.59C18.05,7.91 19,9.9 19,12A7,7 0 0,1 12,19C8.14,19 5,15.88 5,12C5,9.91 5.95,7.91 7.58,6.58L6.17,5.17C2.38,8.39 1.92,14.07 5.14,17.86C8.36,21.64 14.04,22.1 17.83,18.88C19.85,17.17 21,14.65 21,12C21,9.37 19.84,6.87 17.83,5.17Z" /></svg>`
 startButton.classList.add('start-btn')
 startButton.innerHTML = 'START ' + startIcon
+
+const reStartButton = document.createElement('button');
+    const restartIcon = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><title>restart</title><path d="M12,4C14.1,4 16.1,4.8 17.6,6.3C20.7,9.4 20.7,14.5 17.6,17.6C15.8,19.5 13.3,20.2 10.9,19.9L11.4,17.9C13.1,18.1 14.9,17.5 16.2,16.2C18.5,13.9 18.5,10.1 16.2,7.7C15.1,6.6 13.5,6 12,6V10.6L7,5.6L12,0.6V4M6.3,17.6C3.7,15 3.3,11 5.1,7.9L6.6,9.4C5.5,11.6 5.9,14.4 7.8,16.2C8.3,16.7 8.9,17.1 9.6,17.4L9,19.4C8,19 7.1,18.4 6.3,17.6Z" /></svg>`
+    reStartButton.classList.add('restart-btn')
+    reStartButton.innerHTML = 'RESTART ' + restartIcon;
+    reStartButton.classList.add('hidden'); //hide restart button on load
 
 sideBarDiv.append(infoParagraph, startButton);
 
@@ -62,10 +67,7 @@ const DisplayController = (() => {
 
     const gameInterfaceDiv = document.createElement('div');
     gameInterfaceDiv.classList.add ('content');
-    const reStartButton = document.createElement('button');
-    const restartIcon = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><title>restart</title><path d="M12,4C14.1,4 16.1,4.8 17.6,6.3C20.7,9.4 20.7,14.5 17.6,17.6C15.8,19.5 13.3,20.2 10.9,19.9L11.4,17.9C13.1,18.1 14.9,17.5 16.2,16.2C18.5,13.9 18.5,10.1 16.2,7.7C15.1,6.6 13.5,6 12,6V10.6L7,5.6L12,0.6V4M6.3,17.6C3.7,15 3.3,11 5.1,7.9L6.6,9.4C5.5,11.6 5.9,14.4 7.8,16.2C8.3,16.7 8.9,17.1 9.6,17.4L9,19.4C8,19 7.1,18.4 6.3,17.6Z" /></svg>`
-    reStartButton.classList.add('restart-btn')
-    reStartButton.innerHTML = 'RESTART ' + restartIcon;
+    
 
     //function to create 3x3 grid
     const createGrid = () => {
@@ -120,6 +122,7 @@ const DisplayController = (() => {
         });
         if(reStartButton){
             reStartButton.addEventListener('click', () => {
+                reStartButton.classList.add("hidden");
                 GameController.resetGame();
                 updateDisplay();
             });
@@ -166,6 +169,7 @@ const GameController = (() => {
             isGameOver = false;
 
             sideBarDiv.classList.add('hidden');
+            reStartButton.classList.remove("hidden");
             
             DisplayController.updateStatus(`${players[activePlayerIndex].name}'s Turn`);
 
